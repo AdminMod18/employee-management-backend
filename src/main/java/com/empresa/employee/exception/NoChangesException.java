f81ec53eaca0ce4b37f7be75be88c9623dad1733
AdministradorMod18 <hugoa.latorrep@konradlorenz.edu.co>
2026-07-21 22:43:58 -0500

    feat: agregar NoChangesException cuando una actualizacion no modifica datos

---
+++ src/main/java/com/empresa/employee/exception/NoChangesException.java
@@ -0,0 +1,8 @@
+package com.empresa.employee.exception;
+
+public class NoChangesException extends RuntimeException {
+
+    public NoChangesException(String message) {
+        super(message);
+    }
+}
