c6838688e9bcf9d1b956356fba9015a3a30f5868
AdministradorMod18 <hugoa.latorrep@konradlorenz.edu.co>
2026-07-21 22:43:41 -0500

    feat: modelar entidad Departamento con anotaciones JPA

---
+++ src/main/java/com/empresa/employee/entity/Departamento.java
@@ -0,0 +1,51 @@
+package com.empresa.employee.entity;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "departamento")
+public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @OneToMany(mappedBy = "departamento", cascade = CascadeType.ALL, orphanRemoval = false)
+    private List<Empleado> empleados = new ArrayList<>();
+
+    public Departamento() {
+    }
+
+    public Departamento(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+}
