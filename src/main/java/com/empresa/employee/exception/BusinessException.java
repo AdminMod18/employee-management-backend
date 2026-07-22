44362cc67920fd3699cbaec26014c029636384ac
AdministradorMod18 <hugoa.latorrep@konradlorenz.edu.co>
2026-07-21 22:43:58 -0500

    feat: agregar BusinessException para reglas de negocio

---
+++ src/main/java/com/empresa/employee/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.empresa.employee.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
